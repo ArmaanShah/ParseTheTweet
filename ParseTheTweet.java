d323e4c49d10bf9b38756fda7b3f9c4fc219f297
ArmaanShah <69207811+ArmaanShah@users.noreply.github.com>
2024-12-26 21:07:46 -0500

    Update ParseTheTweet.java

--- ParseTheTweet.java
+++ ParseTheTweet.java
@@ -6,20 +6,7 @@
  * Purpose:The following program parses through a tweet that is inputted by a user and divides up to information into sections based on the type of information. 
  * Each one of these sections is then manipulated to make it easier to read, and the results are then printed. 
  * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither 
- * received nor given inappropriate assistance. I have not copied 
- * or modified code from any source other than the course webpage 
- * or the course textbook. I recognize that any unauthorized 
- * assistance or plagiarism will be handled in accordance with 
- * the University of Georgia's Academic Honesty Policy and the 
- * policies of this course. I recognize that my work is based 
- * on an assignment created by the School of Computing 
- * at the University of Georgia. Any publishing or
- * posting of source code for this assignment is strictly 
- * prohibited unless you have written consent from the 
- * School of Computing at the University of Georgia.  
- */
+
 
 import java.util.Scanner;
 
